8300a8424960e70d21dd6a59411e9251812a67e5
Кирилл Дегтярев <kirill.degtyar76@gmail.com>
2022-10-24 20:08:04 +0300

    Updates

--- precache-manifest.5c7952d85fe319b0ae9e4f89ca8d0a37.js
+++ precache-manifest.5c7952d85fe319b0ae9e4f89ca8d0a37.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "revision": "bbc6af1af33d08c45dcc",
-    "url": "./static/css/main.82790a0c.chunk.css"
+    "revision": "79ebae245adcac4120dd",
+    "url": "./static/css/main.9081c894.chunk.css"
   },
   {
-    "revision": "bbc6af1af33d08c45dcc",
-    "url": "./static/js/main.bbc6af1a.chunk.js"
+    "revision": "79ebae245adcac4120dd",
+    "url": "./static/js/main.79ebae24.chunk.js"
   },
   {
-    "revision": "e87c296d29faa82a105e",
-    "url": "./static/js/1.e87c296d.chunk.js"
+    "revision": "8c69d45b4e44187eea66",
+    "url": "./static/js/1.8c69d45b.chunk.js"
   },
   {
     "revision": "4a686d48d5a089750c49",
@@ -24,47 +24,47 @@ self.__precacheManifest = [
     "url": "./static/media/plus.3d9f3c43.svg"
   },
   {
-    "revision": "5fe650197a70462aa000e971c52a2562",
-    "url": "./static/media/proxima_nova_semibold.5fe65019.woff2"
-  },
-  {
-    "revision": "5082ba8e0c15946eb97e8394488c6d36",
-    "url": "./static/media/proxima_nova_semibold.5082ba8e.woff"
-  },
-  {
     "revision": "b7c512788e3c77b0196f0bace8a88418",
     "url": "./static/media/proxima_nova_regular.b7c51278.eot"
+  },
+  {
+    "revision": "5fe650197a70462aa000e971c52a2562",
+    "url": "./static/media/proxima_nova_semibold.5fe65019.woff2"
   },
   {
     "revision": "cddac6589ca25eb44179341774222e79",
     "url": "./static/media/proxima_nova_semibold.cddac658.eot"
   },
   {
-    "revision": "13ac2738feb0bee6658ff70771ab8998",
-    "url": "./static/media/proxima_nova_regular.13ac2738.woff2"
-  },
-  {
     "revision": "df8c626474a73ab7a8b511655597c7c4",
     "url": "./static/media/proxima_nova_semibold.df8c6264.ttf"
+  },
+  {
+    "revision": "5082ba8e0c15946eb97e8394488c6d36",
+    "url": "./static/media/proxima_nova_semibold.5082ba8e.woff"
   },
   {
     "revision": "3780411ec06c6e8b459605d8a5ea3a04",
     "url": "./static/media/proxima_nova_semibold.3780411e.svg"
   },
   {
-    "revision": "61571ff74cc0fba6de49ace74846bfec",
-    "url": "./static/media/proxima_nova_regular.61571ff7.woff"
+    "revision": "13ac2738feb0bee6658ff70771ab8998",
+    "url": "./static/media/proxima_nova_regular.13ac2738.woff2"
   },
   {
     "revision": "7ce6760d17685c466ba04d1b2c63c38b",
     "url": "./static/media/proxima_nova_regular.7ce6760d.ttf"
   },
   {
+    "revision": "61571ff74cc0fba6de49ace74846bfec",
+    "url": "./static/media/proxima_nova_regular.61571ff7.woff"
+  },
+  {
     "revision": "91c478abc3c2b0ee83469d351b30393b",
     "url": "./static/media/proxima_nova_regular.91c478ab.svg"
   },
   {
-    "revision": "f7e33d6b32356b34b0552c4b04eec26a",
+    "revision": "b12dbd18ebfe09a3b82f1843b0e56783",
     "url": "./index.html"
   }
 ];
